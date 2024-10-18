c83ce859c2bf5d5dca6fdf448b9a7edbd6212e66
Piotr Merski <merskip@gmail.com>
2024-10-18 07:30:45 +0200

    Added moving player

---
+++ Models/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed: float = 300.0
+
+func _physics_process(delta: float) -> void:
+	var move = Vector3(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down"),
+		0
+	).normalized()
+	velocity.x = move.x * speed
+	velocity.y = move.y * speed
+
+	move_and_slide()
