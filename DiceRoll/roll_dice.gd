c2ad5b8fc589c7095a69f14542d1342452961d3b
Piotr Merski <merskip@gmail.com>
2024-10-23 14:22:27 +0200

    Added support for roll multiple dices

--- DiceRoll/roll_dice.gd
+++ DiceRoll/roll_dice.gd
@@ -7,63 +7,58 @@ enum DiceType {
 	D8,
 	D10,
 	D12,
-	D20,
-	D100
+	D20
 }
 
-@export var dice_type: DiceType = DiceType.D6
+var _dices_types: Array[DiceType] = []
+var _dices_bodies: Array[RigidBody3D] = []
+
 @export var rotation_speed_max: float = 10.0
 @onready var start_marker: Node3D = $StartPosition
-@onready var label := $CanvasLayer/Label
+@onready var roll_result_label := %RollResult
 @onready var dice_roll_start_sfx: AudioStreamPlayer = $DiceRollStart
 @onready var dice_rolling_sfx: AudioStreamPlayer = $DiceRolling
 
 var _rolling_on_ground = false
-var dice: RigidBody3D
 
-signal on_rolling_on_ground()
+
+var dices_scene := preload("res://DiceRoll/dices.tscn")
+
 signal on_roll_result(value: int)
 
 func _on_d4_button_pressed() -> void:
-	set_dice_type(DiceType.D4)
+	add_dice(DiceType.D4)
 
 func _on_d6_button_pressed() -> void:
-	set_dice_type(DiceType.D6)
+	add_dice(DiceType.D6)
 
 func _on_d8_button_pressed() -> void:
-	set_dice_type(DiceType.D8)
+	add_dice(DiceType.D8)
 
 func _on_d10_button_pressed() -> void:
-	set_dice_type(DiceType.D10)
+	add_dice(DiceType.D10)
 
 func _on_d12_button_pressed() -> void:
-	set_dice_type(DiceType.D12)
+	add_dice(DiceType.D12)
 
 func _on_d20_button_pressed() -> void:
-	set_dice_type(DiceType.D20)
+	add_dice(DiceType.D20)
 
-func set_dice_type(dice_type: DiceType):
-	self.dice_type = dice_type
-	reload_dice()
-
-func _ready() -> void:
-	reload_dice()
+func add_dice(dice_type: DiceType):
+	var dice_body = load_dice(dice_type)
+	dice_body.get_parent().remove_child(dice_body)
+	dice_body.global_position = start_marker.global_position
+	dice_body.freeze = true
+	add_child(dice_body)
+	dice_body.sleeping_state_changed.connect(_on_dice_sleeping_state_changed.bind(dice_body))
+	dice_body.body_entered.connect(_on_dice_body_entered)
 	
-func reload_dice():
-	if dice:
-		remove_child(dice)
-	var new_dice = load_dice()
-	new_dice.get_parent().remove_child(new_dice)
-	new_dice.global_position = start_marker.global_position
-	new_dice.freeze = true
-	add_child(new_dice)
-	new_dice.sleeping_state_changed.connect(_on_dice_sleeping_state_changed)
-	new_dice.body_entered.connect(_on_dice_body_entered)
-	dice = new_dice
+	_dices_types.append(dice_type)
+	_dices_bodies.append(dice_body)
 	
 
-func load_dice() -> RigidBody3D:
-	var dices = preload("res://DiceRoll/dices.tscn").instantiate()
+func load_dice(dice_type: DiceType) -> RigidBody3D:
+	var dices := dices_scene.instantiate()
 	match dice_type:
 		DiceType.D4:
 			return dices.get_node("DiceD4")
@@ -77,59 +72,73 @@ func load_dice() -> RigidBody3D:
 			return dices.get_node("DiceD12")
 		DiceType.D20:
 			return dices.get_node("DiceD20")
-		DiceType.D100:
-			return dices.get_node("DiceD100")
-	return null
+		_:
+			push_error("Unknown dice_type: %s" % dice_type)
+			return null
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		roll()
 		
 func _process(_delta: float) -> void:
-	var top_side = get_top_side_value()
-	
+	roll_result_label.text = ""
+	for i in range(_dices_types.size()):
+		var dice_type := _dices_types[i]
+		var dice_body := _dices_bodies[i]
+		
+		var dice_value = get_top_side_value(dice_body)
+		roll_result_label.text += dice_name(dice_type) + ": "
+		
+		if dice_body.sleeping:
+			roll_result_label.text += "rolled %s\n" % [dice_value]
+		elif dice_value != -1:
+			roll_result_label.text += "%s (rolling)\n" % dice_value
+		else:
+			roll_result_label.text += "? (rolling)\n"
+
+
+func dice_name(dice_type: DiceType) -> String:
 	match dice_type:
 		DiceType.D4:
-			label.text = "D4"
+			return "D4"
 		DiceType.D6:
-			label.text = "D6"
+			return  "D6"
 		DiceType.D8:
-			label.text = "D8"
+			return  "D8"
 		DiceType.D10:
-			label.text = "D10"
+			return "D10"
 		DiceType.D12:
-			label.text = "D12"
+			return "D12"
 		DiceType.D20:
-			label.text = "D20"
-		DiceType.D100:
-			label.text = "D100"
-	if dice.sleeping:
-		label.text += ": rolled %s" % [top_side]
-	else:
-		label.text += ": %s (rolling)\n" % top_side
-	
+			return "D20"
+		_:
+			push_error("Unknown dice_type: %s" % dice_type)
+			return ""
 
 func roll() -> void:
-	dice.freeze = true
-	dice.position = start_marker.position
-	dice.rotation = Vector3(randf_range(-2 * PI, 2 * PI),
-							randf_range(-2 * PI, 2 * PI),
-							randf_range(-2 * PI, 2 * PI))
-	dice.angular_velocity = Vector3(randf_range(-rotation_speed_max, rotation_speed_max),
-									randf_range(-rotation_speed_max, rotation_speed_max),
-									randf_range(-rotation_speed_max, rotation_speed_max))
-	dice.freeze = false
 	_rolling_on_ground = false
+	for dice_body in _dices_bodies:
+		_roll_dice(dice_body)
 	dice_roll_start_sfx.play()
 
-func _on_dice_sleeping_state_changed() -> void:
-	if dice.sleeping:
-		var value := get_top_side_value()
+func _roll_dice(dice_body: RigidBody3D):
+	dice_body.position = start_marker.position
+	dice_body.rotation = Vector3(randf_range(-2 * PI, 2 * PI),
+								 randf_range(-2 * PI, 2 * PI),
+								 randf_range(-2 * PI, 2 * PI))
+	dice_body.angular_velocity = Vector3(randf_range(-rotation_speed_max, rotation_speed_max),
+										 randf_range(-rotation_speed_max, rotation_speed_max),
+										 randf_range(-rotation_speed_max, rotation_speed_max))
+	dice_body.freeze = false
+
+func _on_dice_sleeping_state_changed(dice_body: RigidBody3D) -> void:
+	if dice_body.sleeping:
+		var value := get_top_side_value(dice_body)
 		on_roll_result.emit(value)
 		dice_roll_start_sfx.stop()
 		dice_rolling_sfx.stop()
 
-func get_top_side_value() -> int:
+func get_top_side_value(dice: RigidBody3D) -> int:
 	var raycasts = dice.get_node("RayCasts").get_children()
 	for raycast in raycasts:
 		if raycast.is_colliding():
@@ -140,5 +149,4 @@ func get_top_side_value() -> int:
 func _on_dice_body_entered(_body: Node) -> void:
 	if not _rolling_on_ground:
 		_rolling_on_ground = true
-		on_rolling_on_ground.emit()
 		dice_rolling_sfx.play()
